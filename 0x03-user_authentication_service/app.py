761bc81e1cdba2a693174b800c1202b3d0835796
bion-slmn <bionsol25@gmail.com>
2024-02-22 09:13:04 +0000

    returning with correct spacing

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,7 +82,7 @@ def profile() -> Response:
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
     if user:
-        return jsonify({"email": user.email}), 200
+        return jsonify({"email": f"{user.email}"}), 200
     abort(403)
 
 
